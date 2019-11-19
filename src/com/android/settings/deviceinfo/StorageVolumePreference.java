Havoc-OS/android_packages_apps_Settings

SettingsLib: Update Utils APIs usage
Update Utils.getColorAttr and similar methods in Utils to return a ColorStateList instead of a color

Bug: 74872054
Test: Force to display different color items then manually test
Change-Id: If002f9a578558e6254220c36ef556e8378d4522a

src/com/android/settings/deviceinfo/StorageVolumePreference.java
@@ -61,7 +61,7 @@ public class StorageVolumePreference extends Preference {
         mVolume = volume;
         mColor = color;
 
-        mIconColor = Utils.getColorAttr(context, android.R.attr.colorControlNormal);
+        mIconColor = Utils.getColorAttrDefaultColor(context, android.R.attr.colorControlNormal);
 
         setLayoutResource(R.layout.storage_volume);
 
@@ -109,7 +109,7 @@ public class StorageVolumePreference extends Preference {
             }
 
             if (freeBytes < mStorageManager.getStorageLowBytes(path)) {
-                mIconColor = Utils.getColorAttr(context, android.R.attr.colorError);
+                mIconColor = Utils.getColorAttrDefaultColor(context, android.R.attr.colorError);
                 icon = context.getDrawable(R.drawable.ic_warning_24dp);
             }
 

Bootleg-P/packages_apps_Settings

src/com/android/settings/deviceinfo/StorageVolumePreference.java
@@ -108,7 +108,7 @@ public class StorageVolumePreference extends Preference {
             }
 
             if (freeBytes < mStorageManager.getStorageLowBytes(path)) {
-                mColor = Utils.getColorAttr(context, android.R.attr.colorError);
+                mColor = Utils.getColorAttrDefaultColor(context, android.R.attr.colorError);
                 icon = context.getDrawable(R.drawable.ic_warning_24dp);
             }
 